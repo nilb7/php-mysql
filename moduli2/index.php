6bd348519ee020d824747ffae6e54b67aefc31b2
nilb7 <tieenn298@gmail.com>
2025-01-23 16:14:01 +0100

    erdf

---
+++ moduli2/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$andi='Andi';
+$mosha=15;
+$height=1.87;
+$blabla=false;
+
+
+ echo $andi;
+ echo $mosha;
+ echo $height;
+ echo $blabla;
+
+ $x = 127;
+ $y = 20;
+
+ echo $x + $y;
+
+$andiii "Andi ma i forti"
+
+ echo strlen($andiii)
+ echo str_word_count($andiii)
+  echo strrev ($andiii)
+  echo str_replace($andiii)
+
+ ?>
