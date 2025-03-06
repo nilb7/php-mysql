8e3ce5ea4f35a479fb2c74128e11b75736370d23
nilb7 <tieenn298@gmail.com>
2025-03-06 16:13:31 +0100

    SDDsdsd

--- moduli9/add.php
+++ moduli9/add.php
@@ -2,18 +2,18 @@
 
 require('config.php');
 if(isset($_POST['submit'])){
-    $name = $_POST['name']
-    $username = $_POST['username']
-    $email = $_POST['email']
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
     $sql = 'INSERT INTO users(name,username,email)
-    VALVES(NAME, USERNAME ,EMAIL)';
+    VALUES(:name, :username ,:email)';
 
-    
+
     $sqlQuery = $connection -> prepare ($sql);
     $sqlQuery -> bindParam(':name','$name');
     $sqlQuery -> bindParam(':username','$username');
     $sqlQuery -> bindParam(':email','$email');
-
+    $sqlQuery->execute();
 }
 
 
