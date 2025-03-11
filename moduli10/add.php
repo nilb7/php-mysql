3408f7e755badceed5928d5b1ab54dbdd7f6a280
nilb7 <tieenn298@gmail.com>
2025-03-11 16:08:46 +0100

    dsdsd

---
+++ moduli10/add.php
@@ -0,0 +1,20 @@
+<?php
+
+require('config.php');
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $sql = 'INSERT INTO users(name,username,email)
+    VALUES(:name, :username ,:email)';
+
+
+    $sqlQuery = $connection -> prepare ($sql);
+    $sqlQuery -> bindParam(':name','$name');
+    $sqlQuery -> bindParam(':username','$username');
+    $sqlQuery -> bindParam(':email','$email');
+    $sqlQuery->execute();
+}
+
+
+?>
