a5ffd2c498903503e249a9ec946b1377e3a75e1e
nilb7 <tieenn298@gmail.com>
2025-03-13 16:03:51 +0100

    sdsd

---
+++ moduli11/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+     <form action="add.php" method="Post">
+
+    <input type="text" id="name" name="name" placeholder="Name"><br>
+    <input type="text" id="surname" name="surname" placeholder="Surname"><br>
+    <input type="email" id="email" name="email" placeholder="Email"><br>
+    <input type="number" id="age" name="age" placeholder="Age"><br>
+    <button type="submit" name="submit">Add</button>
+</form>
+</body>
+</html>
