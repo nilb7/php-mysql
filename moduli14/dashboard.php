0da10e9d44d4682a699aa09429debbb4dc983ff2
nilb7 <tieenn298@gmail.com>
2025-04-03 16:15:42 +0200

    gg

--- moduli14/dashboard.php
+++ moduli14/dashboard.php
@@ -35,6 +35,16 @@
                     <a href="logout.php" class="nav-link">Sign out</a>
                 </ul>
         </nav>
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <ul class="sidebar-sticky">
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
 
         <?php
     include_once('config.php');
