b2ff683d4ccfa953e34e1dad05977dad075c0f83
nilb7 <tieenn298@gmail.com>
2025-05-06 15:18:49 +0200

    gg

--- MMS_PROJECT/list_movies.php
+++ MMS_PROJECT/list_movies.php
@@ -29,6 +29,7 @@
  <html>
  <head>
   <title>Dashboard</title>
+  <a href="movies.php" class="btn btn-primary">Add Movie</a>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
